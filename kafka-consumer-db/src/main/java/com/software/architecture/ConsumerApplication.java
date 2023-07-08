2e7c3dde9779cb58947f214140ebf00d2adc83b7
Riham Nour <riham.nour@carrier.com>
2023-07-08 16:29:14 +0300

    cleanup

---
+++ kafka-consumer-db/src/main/java/com/software/architecture/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.software.architecture;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
+
